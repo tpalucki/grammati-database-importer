d58f42ce64262c8e2a2eaeb7c2a10cae52e524db
Tomasz Pałucki <tpalucki@gmail.com>
2021-02-09 23:07:54 +0100

    Initial commit

---
+++ src/main/java/io/tpalucki/grammati/questionimporter/QuestionImporterApplication.java
@@ -0,0 +1,13 @@
+package io.tpalucki.grammati.questionimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionImporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/tpalucki/grammati/questionimporter/QuestionImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tpalucki.grammati.questionimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
